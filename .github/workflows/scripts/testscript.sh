4e8b01e1b08ca2ada73ed8c3aa0c2f67b968c0a4
balaji-m <balaji.m89@gmail.com>
2023-10-23 20:43:48 +0530

    Update testscript.sh

--- .github/workflows/scripts/testscript.sh
+++ .github/workflows/scripts/testscript.sh
@@ -9,6 +9,18 @@ fi
 # JSON input file
 json_file="config.json"
 
+# Check if the email array is not empty
+if [ $(jq -r '.email | length' "$json_file") -eq 0 ]; then
+    echo "Email array is empty."
+    exit 1
+fi
+
+# Check if the Splunk index is not empty
+if [ -z $(jq -r '.splunk_index' "$json_file") ]; then
+    echo "Splunk index is empty."
+    exit 1
+fi
+
 # Validate the JSON error codes if enable_all is false
 if [ $(jq -r '.error_codes.enable_all' "$json_file") == "false" ]; then
     if jq -e '.error_codes.codes | all(. == 400 or . == 401 or . == 500)' "$json_file"; then
