8295e9171941130951b9e99cd88aa016049d0b24
balaji-m <balaji.m89@gmail.com>
2023-10-23 20:04:06 +0530

    Update testscript.sh

--- .github/workflows/scripts/testscript.sh
+++ .github/workflows/scripts/testscript.sh
@@ -9,11 +9,32 @@ fi
 # JSON input file
 json_file="config.json"
 
-# Validate the JSON and allow only codes 400 and 401
-if jq -e '.error_codes.codes | all(. == 400 or . == 401 or . == 500)' "$json_file"; then
-    echo "JSON validation successful. It contains only allowed status codes (400 and 401)."
-    exit 0
-else
-    echo "JSON validation failed. It contains disallowed status codes."
+# Check if the email array is not empty
+if [ $(jq -r '.email | length' "$json_file") -eq 0 ]; then
+    echo "Email array is empty."
     exit 1
+fi
+
+# Check if the Splunk index is not empty
+if [ -z "$(jq -r '.splunk_index' "$json_file") ]; then
+    echo "Splunk index is empty."
+    exit 1
+fi
+
+# Validate the JSON error codes if enable_all is false
+if [ $(jq -r '.error_codes.enable_all' "$json_file") == "false" ]; then
+    if jq -e '.error_codes.codes | all(. == 400 or . == 401 or . == 500)' "$json_file"; then
+        echo "JSON validation successful. It contains only allowed status codes (400, 401, 500)."
+    else
+        echo "JSON validation failed. It contains disallowed status codes."
+        exit 1
+    fi
+else
+    echo "JSON validation skipped for error codes. enable_all is set to true."
+fi
+
+# Check if the business alerts array has a specific size (e.g., 1)
+if [ $(jq -r '.business_alerts | length' "$json_file") -ne 0 ]; then
+    echo "Business alerts present."
+    exit 0
 fi
