ed7764be5f3f65d95df5e10e1560a8bb9fc537b2
balaji-m <balaji.m89@gmail.com>
2023-10-23 20:40:09 +0530

    Update testscript.sh

--- .github/workflows/scripts/testscript.sh
+++ .github/workflows/scripts/testscript.sh
@@ -9,18 +9,6 @@ fi
 # JSON input file
 json_file="config.json"
 
-# Check if the email array is not empty
-if [ $(jq -r '.email | length' "$json_file") -eq 0 ]; then
-    echo "Email array is empty."
-    exit 1
-fi
-
-# Check if the Splunk index is not empty
-if [ -z "$(jq -r '.splunk_index' "$json_file") ]; then
-    echo "Splunk index is empty."
-    exit 1
-fi
-
 # Validate the JSON error codes if enable_all is false
 if [ $(jq -r '.error_codes.enable_all' "$json_file") == "false" ]; then
     if jq -e '.error_codes.codes | all(. == 400 or . == 401 or . == 500)' "$json_file"; then
@@ -32,3 +20,10 @@ if [ $(jq -r '.error_codes.enable_all' "$json_file") == "false" ]; then
 else
     echo "JSON validation skipped for error codes. enable_all is set to true."
 fi
+
+# Check if the business alerts array has a specific size (e.g., 1)
+if [ $(jq -r '.business_alerts | length' "$json_file") -ne 0 ]; then
+    echo "Business alerts present"
+fi
+exit 0
+
