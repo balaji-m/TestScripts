f5b8f90c598bdc7df9adcca9fe62a51c3373cacb
balaji-m <balaji.m89@gmail.com>
2023-12-12 15:53:37 +0530

    Update terraform-api.sh

--- .github/workflows/scripts/terraform-api.sh
+++ .github/workflows/scripts/terraform-api.sh
@@ -28,31 +28,31 @@ WORKSPACE_ID=($(curl \
 
 echo "step3 completed: $WORKSPACE_ID"
 
-# 4. Create a New Configuration Version
+# # 4. Create a New Configuration Version
 
-echo '{"data":{"type":"configuration-versions"}}' > ./create_config_version.json
+# echo '{"data":{"type":"configuration-versions"}}' > ./create_config_version.json
 
-UPLOAD_URL=($(curl \
-  --header "Authorization: Bearer $TOKEN" \
-  --header "Content-Type: application/vnd.api+json" \
-  --request POST \
-  --data @create_config_version.json \
-  https://app.terraform.io/api/v2/workspaces/$WORKSPACE_ID/configuration-versions \
-  | jq -r '.data.attributes."upload-url"'))
+# UPLOAD_URL=($(curl \
+#   --header "Authorization: Bearer $TOKEN" \
+#   --header "Content-Type: application/vnd.api+json" \
+#   --request POST \
+#   --data @create_config_version.json \
+#   https://app.terraform.io/api/v2/workspaces/$WORKSPACE_ID/configuration-versions \
+#   | jq -r '.data.attributes."upload-url"'))
 
-  echo "step4 completed: $UPLOAD_URL"
+#   echo "step4 completed: $UPLOAD_URL"
 
-# 5. Upload the Configuration Content File
+# # 5. Upload the Configuration Content File
 
-curl \
-  --header "Content-Type: application/octet-stream" \
-  --request PUT \
-  --data-binary @"$UPLOAD_FILE_NAME" \
-  $UPLOAD_URL
+# curl \
+#   --header "Content-Type: application/octet-stream" \
+#   --request PUT \
+#   --data-binary @"$UPLOAD_FILE_NAME" \
+#   $UPLOAD_URL
 
- echo "step5 completed"
+#  echo "step5 completed"
 
-# 6. Delete Temporary Files
+# # 6. Delete Temporary Files
 
-rm "$UPLOAD_FILE_NAME"
-rm ./create_config_version.json
+# rm "$UPLOAD_FILE_NAME"
+# rm ./create_config_version.json
